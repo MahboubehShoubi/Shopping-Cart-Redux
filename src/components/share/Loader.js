832c30caf1de94b38e1c6514fbaeb4e8ca9ce21b
mahboubeh shoubi <mahboobeh.shobi.65@gmail.com>
2023-08-27 13:45:41 +0330

    Create Loader.module.css and styled to Loader.js

--- src/components/share/Loader.js
+++ src/components/share/Loader.js
@@ -3,9 +3,12 @@ import React from 'react';
 //Images
 import spinner from "../../assets/images/Spinner.gif";
 
+//css
+import styles from "../../css/share/Loader.module.css";
+
 const Loader = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <img src={spinner} alt="spinner"/>
         </div>
     );
