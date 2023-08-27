bfb639e99d5d780087a7dbf996e34b3c882088bc
mahboubeh shoubi <mahboobeh.shobi.65@gmail.com>
2023-08-27 22:13:58 +0330

    Create Product.js and Product.module.css and styled to product.js

--- src/components/share/Product.js
+++ src/components/share/Product.js
@@ -11,29 +11,38 @@ import { addItem , removeItem , increase , decrease} from "../../redux/cart/cart
 //Icons
 import trashIcon from "../../assets/icons/trash.svg";
 
+//css
+import styles from "../../css/share/Product.module.css";
+
 const Product = ({productData}) => {
+
 
     const dispatch = useDispatch();
     const state = useSelector(state => state.cartState);
 
     return (
-        <div>
-            <img src={productData.image} alt="product" />
-            <h3>{shorten(productData.title)}</h3>
-            <p>{`${productData.price} $`}</p>
-            <div>
-                <Link to={`/products/${productData.id}`}>Details</Link>
-                <div>
-                    {quantityCount(state, productData.id) === 1 && <button onClick={() => dispatch(removeItem(productData))}><img src={trashIcon} alt="trash" /></button>}
-                    {quantityCount(state, productData.id) > 1 && <button onClick={() => dispatch(decrease(productData))}>-</button>}
-                    {quantityCount(state, productData.id) > 0 && <span >{quantityCount(state, productData.id)}</span>}
-                    {
-                        isInCart(state, productData.id) ?
-                            <button  onClick={() => dispatch(increase(productData))}>+</button> :
-                            <button onClick={() => dispatch(addItem(productData))}>Add to Cart</button>
-                    }
+        <div className={styles.container}>
+            <div className={styles.containerImg}>
+                <img src={productData.image} alt="product" />
+            </div>
+            <div className={styles.containerInfo}>
+                <h3 className={styles.productTitle}>{shorten(productData.title)}</h3>
+                <p><span className={styles.price}>Price : </span>{`${productData.price} $`}</p>
+                <div className={styles.control}>
+                    <Link className={styles.productLink} to={`/products/${productData.id}`}>Details</Link>
+                    <div className={styles.controlBox}>
+                        {quantityCount(state, productData.id) === 1 && <button className={styles.removeItem} onClick={() => dispatch(removeItem(productData))}><img src={trashIcon} alt="trash" /></button>}
+                        {quantityCount(state, productData.id) > 1 && <button className={styles.decrease} onClick={() => dispatch(decrease(productData))}>-</button>}
+                        {quantityCount(state, productData.id) > 0 && <span className={styles.quantityCount}>{quantityCount(state, productData.id)}</span>}
+                        {
+                            isInCart(state, productData.id) ?
+                                <button className={styles.increase} onClick={() => dispatch(increase(productData))}>+</button> :
+                                <button className={styles.addItem} onClick={() => dispatch(addItem(productData))}>Add to Cart</button>
+                        }
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 };
